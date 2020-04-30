2bd0f0dd0261ff775340fc12ca294d78feabf60a
Alexandr <alekspetrofint@gmail.com>
2020-04-30 11:30:43 +0300

    RBAC

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'supportEmail' => 'robot@devreadwrite.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+];
